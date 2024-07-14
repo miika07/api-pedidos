9768b29385b05c5e9008b8536542ea79cbc41ca1
Camila Couto <camilagcouto@hotmail.com>
2024-07-14 16:53:27 -0300

    adicionando testes

--- src/infra/data/database/data-source-teste.ts
+++ src/infra/data/database/data-source-teste.ts
@@ -1,17 +1,18 @@
 import { DataSource } from "typeorm";
 
 export const AppDataSourceTest = new DataSource({
-    type: "mysql",
+    type: "mongodb",
     host: "localhost",
-    port: 3307,
-    username: "fiap-test",
+    // host: "mongodbtest",
+    port: 27018,
+    username: "fiap",
     password: "password",
     database: "tech-challenge-fiap-test",
-    logging: true,
-    entities: ["src/core/domain/entities/*.ts"],
-    migrations: ["src/infra/data/database/migrations/*.ts"],
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     synchronize: true,
-    dropSchema: true
-})
-
-  
+    logging: true,
+    entities: ["./src/core/domain/entities/*.ts"],
+    subscribers: [],
+    migrations: ["./src/infra/data/database/migrations/*.ts"]
+  });
