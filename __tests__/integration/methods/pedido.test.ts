aa1c109fdb91896b2560bddb2cc1f06537ade792
Camila Couto <camilagcouto@hotmail.com>
2024-07-14 21:24:12 -0300

    fix done

--- __tests__/integration/methods/pedido.test.ts
+++ __tests__/integration/methods/pedido.test.ts
@@ -27,7 +27,7 @@ import { v4 as uuidv4 } from 'uuid';
 //       expect(responsePedido.payload.itensPedido).toHaveLength(2)
 //   });
 
-  it('[POST] Erro ao adicionar um pedido vazio- 400', async () => {
+  it('[POST] Erro ao adicionar um pedido vazio- 400', async (done) => {
     //adicionando pedido
     const paramsPedido: TestRouteOptions = {
         method: 'POST',
@@ -41,7 +41,8 @@ import { v4 as uuidv4 } from 'uuid';
       };
       const responsePedido = await route(paramsPedido);
       expect(responsePedido.statusCode).toBe(400);
-      expect(responsePedido.payload.error).toBe('Bad Request')
+      expect(responsePedido.payload.error).toBe('Bad Request');
+      done();
   });
 
   // it('[GET] Buscar todos os pedidos - 200', async () => {
